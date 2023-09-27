b4e91ce271c63c0873d4ad61053932a5ca00b69d
Luis Hernandez <luisernestohernandez1@hotmail.com>
2023-09-27 09:41:24 -0700

    Revert "Merge branch 'main' of https://github.com/LUIS021201/IS-clase-27092023"
    This reverts commit 21f2b3eef925ebc004485396709993ee30a8cfb4, reversing
    changes made to 58bd26ad9f9d7c955815dfa899e0b307804b168c.

--- app.py
+++ app.py
@@ -1,7 +1,3 @@
 print("Hola mundo")
 print("Cambios hechos por luis para hacer conflicto")
 print("Cambios para quitar despues")
-print("Aqui voy a arruinar la segunda linea >:)")
-print("Cambios hechos por luis para hacer conflicto")
-
-
